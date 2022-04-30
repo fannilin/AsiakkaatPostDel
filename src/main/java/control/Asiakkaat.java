be890c452f993723ac28d835e8c9cfc7515915f6
Fanni Lindqvist <Fanni Lindqvist>
2022-04-30 18:02:43 +0300

    Asiakkaat

---
+++ src/main/java/control/Asiakkaat.java
@@ -0,0 +1,105 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import model.Asiakas;
+import model.dao.Dao;
+
+/**
+ * Servlet implementation class Asiakkaat
+ */
+@WebServlet("/asiakkaat/*")
+public class Asiakkaat extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Asiakkaat() {
+        super();
+        System.out.println("Asiakkaat.Asiakkaat()");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Asiakkaat.doGet()");
+		String pathInfo = request.getPathInfo();	//haetaan kutsun polkutiedot, esim. /aalto			
+		System.out.println("polku: "+pathInfo);	
+		String hakusana="";
+		if(pathInfo!=null) {		
+			hakusana = pathInfo.replace("/", "");
+		}		
+		Dao dao = new Dao();
+		ArrayList<Asiakas> asiakkaat = dao.listaaKaikki(hakusana);
+		System.out.println(asiakkaat);
+		String strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.println(strJSON);	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Asiakkaat.doPost()");
+		JSONObject jsonObj = new JsonStrToObj().convert(request); 
+		Asiakas asiakas = new Asiakas();
+		asiakas.setAsiakas_id(0);
+		asiakas.setEtunimi(jsonObj.getString("etunimi"));
+		asiakas.setSukunimi(jsonObj.getString("sukunimi"));
+		asiakas.setPuhelin(jsonObj.getString("puhelin"));
+		asiakas.setSposti(jsonObj.getString("sposti"));
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();
+		if (dao.lisaaAsiakas(asiakas)) {
+			out.println("{\"response\":1}");
+		} else {
+			out.println("\"response\":0}");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Asiakkaat.doPut()");
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Asiakkaat.doDelete()");
+		String pathInfo = request.getPathInfo();	//haetaan kutsun polkutiedot, esim. /ABC-222		
+		System.out.println("polku: "+pathInfo);
+		String delasiakas_id = pathInfo.replace("/", "");		
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();			
+		if(dao.poistaAsiakas(delasiakas_id)){ //metodi palauttaa true/false
+			out.println("{\"response\":1}");  //Auton poistaminen onnistui {"response":1}
+		}else{
+			out.println("{\"response\":0}");  //Auton poistaminen epäonnistui {"response":0}
+		}	
+	}
+}
---
+++ src/main/webapp/listaaasiakkaat.jsp
@@ -0,0 +1,82 @@
+ <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="main.css">
+<title>Asiakkaiden listaus</title>
+</head>
+<body>
+	<table id="listaus">
+		<thead>
+		<tr>
+		<th 	colspan= "5" class ="oikealle"><span id = "uusiAsiakas">Lisää uusi asiakas</span></th>
+			<tr>
+				<th colspan="3" class="oikealle">Hakusana:</th>
+				<th><input type="text" id="hakusana"></th>
+				<th><input type="button" id="hae" value="Hae"></th>
+			</tr>		
+			<tr>
+				<th>Etunimi</th>
+				<th>Sukunimi</th>
+				<th>Puhelin</th>
+				<th>Sposti</th>	
+				<th></th>			
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+	<span id="ilmo"></span>
+<script>
+$(document).ready(function(){	
+	$(document.body).on("keydown", function(event){
+		  if(event.which==13){ //Enter painettu, ajetaan haku
+			  haeTiedot();
+		  }
+	});	
+	$("#uusiAsiakas").click(function(){
+		document.location="lisaaasiakas.jsp";
+	});
+	$("#hae").click(function(){	
+		haeTiedot();
+	});
+	$("#hakusana").focus();
+	haeTiedot();
+});
+function haeTiedot(){	
+	$("#listaus tbody").empty();
+	$.ajax({url:"asiakkaat/"+$("#hakusana").val(), type:"GET", dataType:"json", success:function(result){
+		$.each(result.asiakkaat, function(i, field){  
+        	var htmlStr;
+        	htmlStr+="<tr>"; 
+        	htmlStr+="<td>"+field.etunimi+"</td>";
+        	htmlStr+="<td>"+field.sukunimi+"</td>";
+        	htmlStr+="<td>"+field.puhelin+"</td>";
+        	htmlStr+="<td>"+field.sposti+"</td>";  
+        	htmlStr+="<td><span class='poista' onclick=poista('"+ field.asiakas_id+"')>"Poista"</span></td>";   
+        	htmlStr+="</tr>";
+        	$("#listaus tbody").append(htmlStr);
+        });
+    }});	
+}
+	function poista(asiakas_id){
+	if(confirm("Poista asiakas " + etunimi +"?")){
+		$.ajax({url:"asiakkaat/"+asiakas_id, type:"DELETE", dataType:"json", success:function(result) { //result on joko {"response:1"} tai {"response:0"}
+	        if(result.response==0){
+	        	$("#ilmo").html("Asiakkaan poisto epäonnistui.");
+	        }else if(result.response==1){
+	        	$("#rivi_"+asiakas_id).css("background-color", "orange"); 
+	        	alert("Asiakkaan " + asiakas_id +" poisto onnistui.");
+				haeAsiakkaat();        	
+			}
+	    }});
+	}
+}
+	
+</script>
+</body>
+</html>
+    
